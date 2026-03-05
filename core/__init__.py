0b53316dcf1efd2fe2f001a15c6a0d4d3da9df0e
Mawulolo ALAGBO <parfaitalagbo18@gmail.com>
2026-03-05 09:21:45 +0100

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,6 @@
 """Core module for Data Brain Assistant"""
 from .loader import load_file, infer_types
-from .parser import parse_query, find_column
+from .parser import parse_query, find_column_candidates
 from .validator import validate_plan
 from .executor import execute_action
 
